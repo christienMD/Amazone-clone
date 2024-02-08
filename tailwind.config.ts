04fa403361d8eebcf05c57d762f2f74d1d4b7ada
Md Christien <mdchristien12@gmail.com>
2024-02-08 23:40:45 +0100

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        amazon_blue: {
+          light: "#232F3E",
+          DEFAULT: "#131921",
+        },
+      },
     },
   },
   plugins: [],
