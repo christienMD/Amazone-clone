cadfe5edc5fa738d92b2d64b7cc213e5490b168a
Md Christien <mdchristien12@gmail.com>
2024-02-09 22:33:43 +0100

    refactor product grid layout

--- app/components/ProductFeed.tsx
+++ app/components/ProductFeed.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "./Product";
+import Image from "next/image";
 
 export interface Product {
   id: number;
@@ -16,11 +17,27 @@ const ProductFeed = async () => {
 
   return (
     <div className="grid grid-flow-row-dense md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 md:-mt-52 mx-auto">
-      {products.map((product) => (
+      {products.slice(0, 5).map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
 
+      <Image
+        className="md:col-span-full w-full px-5"
+        src="https://links.papareact.com/dyz"
+        alt=""
+        width={125}
+        height={125}
+        layout="responsive"
+      />
 
+      <div className="md:col-span-2">
+        {products.slice(5, 6).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      {products.slice(6, products.length).map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
     </div>
   );
 };
