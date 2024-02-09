fd264ec6855b7e6b12bd02e5d1ba027c61aeac91
Md Christien <mdchristien12@gmail.com>
2024-02-09 08:35:01 +0100

    build the header navigation

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import Image from "next/image";
+import {
+  ShoppingCartIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      {/* top nav */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2 gap-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            alt="header"
+            src="https://links.papareact.com/f90"
+            height={150}
+            width={40}
+            className="object-contain cursor-pointer"
+          />
+        </div>
 
-export default Header
+        {/* search */}
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+            type="text"
+          />
+          <MagnifyingGlassIcon className="h-12 p-4" />
+        </div>
+
+        {/* right */}
+
+        <div className="text-white flex items-center text-xs space-x-6 mx-6">
+          <div className="link">
+            <p>hello sharon</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded full font-bold text-black">0</span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* bottom nav */}
+      <div className=""></div>
+    </header>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <div className="">
-      <h1>hello, world</h1>
+      <Header />
     </div>
   );
 }
