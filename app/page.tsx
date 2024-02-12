9209862ba419e0eefdbfe7d9d7a5c59dee559754
Md Christien <mdchristien12@gmail.com>
2024-02-13 00:35:46 +0100

    refactore import

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Banner from "./components/Banner";
-import Header from "./components/Header";
-
 import ProductFeed from "./components/ProductFeed";
+
 
 export default function Home() {
   return (
     <div className="bg-gray-100">
-      <Header />
+        
       <main className="max-w-screen-2xl mx-auto">
         {/* Banner */}
         <Banner />
